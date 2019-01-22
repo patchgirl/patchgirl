1c0fb79e7ac9ec2020da3fbd893425faee53e0cc
Iori Matsuhara <matsuhar@gmail.com>
2019-01-22 22:28:08 +0100

    ++

--- elm/Main.elm
+++ elm/Main.elm
@@ -19,7 +19,7 @@ main =
 
 type alias Builders = List(Builder.Model)
 
-type Node = Folder String Tree | File String Builder.Model
+type Node = Folder String Bool Tree | File String Builder.Model
 type alias Tree = List(Node)
 
 type alias Model =
@@ -31,6 +31,7 @@ type alias Model =
 type Msg
   = SetSelectedNode Node
   | SetDisplayedBuilder Int
+  | ToggleNode Int
   | BuilderMsg Builder.Msg
 
 init : () -> (Model, Cmd Msg)
@@ -39,11 +40,11 @@ init _ =
     model =
       { selectedNode = Nothing
       , displayedBuilderIndex = Nothing
-      , tree = [ Folder "folder1" []
-               , Folder "folder2" [ Folder "folder2.2" [] ]
-               , Folder "folder3" <| [ File "file1" Builder.defaultModel1
-                                     , File "file2" Builder.defaultModel2
-                                     ]
+      , tree = [ Folder "folder1" False []
+               , Folder "folder2" True [ Folder "folder2.2" True [] ]
+               , Folder "folder3" True <| [ File "file1" Builder.defaultModel1
+                                          , File "file2" Builder.defaultModel2
+                                          ]
                ]
       }
   in
@@ -68,6 +69,17 @@ update msg model =
 
     SetDisplayedBuilder idx ->
       ( { model | displayedBuilderIndex = Just idx }, Cmd.none)
+
+    ToggleNode idx ->
+      let
+        toggle : Node -> Node
+        toggle node =
+          case node of
+            File _ _ as file -> file
+            Folder name toggleState children as folder ->
+              Folder name (not toggleState) children
+      in
+        ( { model | tree = (modifyNode toggle model.tree idx) }, Cmd.none)
 
 subscriptions : Model -> Sub Msg
 subscriptions _ =
@@ -107,7 +119,7 @@ findNode =
         ([], _) -> (idx, Nothing)
         (node :: tail, _) ->
            case node of
-             Folder _ children  ->
+             Folder _ _ children  ->
                let
                   (newIdx, folderSearch) = find children (idx - 1)
                   (_, tailSearch) = find tail newIdx
@@ -127,12 +139,13 @@ nodeView idx tree =
     [] -> (idx, [])
     node :: tail ->
       case node of
-        Folder name children ->
+        Folder name open children ->
           let
             (folderIdx, folderChildrenView) = nodeView (idx + 1) children
             (newIdx, tailView) = nodeView folderIdx tail
-            folderView = li [] [ b [ ] [ text ("- " ++ name ++ "/") ]
-                               , ul [] folderChildrenView
+            folderToggleView = if open then "-" else "+"
+            folderView = li [] [ b [ onClick (ToggleNode idx) ] [ text (folderToggleView ++ " " ++ name ++ "/") ]
+                               , ul [ hidden (not open) ] folderChildrenView
                                ]
           in
             (newIdx, folderView :: tailView)
@@ -149,3 +162,31 @@ findBuilder tree idx =
   case findNode tree idx of
     Just (File _ builder) -> Just builder
     _ -> Nothing
+
+modifyNode : (Node -> Node) -> Tree -> Int -> Tree
+modifyNode f =
+  let
+    modify : Tree -> Int -> (Int, Tree)
+    modify tree idx =
+      if idx < 0 then
+        (idx, tree)
+      else
+        case (tree, idx) of
+          (node :: tail, 0) -> (-1, (f node) :: tail)
+          ([], _) -> (idx, [])
+          (node :: tail, _) ->
+             case node of
+               Folder name open children  ->
+                 let
+                    (newIdx, newChildren) = modify children (idx - 1)
+                    (rIdx, newTail) = modify tail newIdx
+                    newFolder = Folder name open newChildren
+                 in (rIdx, newFolder :: newTail)
+
+               _ ->
+                 let
+                   (newIdx, newTree) = (modify tail (idx - 1))
+                 in
+                   (newIdx, node :: newTree)
+  in
+    \x y -> modify x y |> Tuple.second
