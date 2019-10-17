3529d951e6390396840cd701319ae2d2e5678f4c
iori <matsuhar@gmail.com>
2019-10-17 11:41:12 +0200

    ++

---
+++ front/generate-elm-client.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE TypeOperators     #-}
+
+import           Servant.API  ((:>), Capture, Get, JSON)
+import           Servant.Elm  (DefineElm (DefineElm), Proxy (Proxy),
+                               defaultOptions, deriveElmDef, defElmImports, defElmOptions,
+                               deriveBoth, generateElmModuleWith)
+import RequestCollection
+import App
+
+deriveElmDef defaultOptions ''RequestCollection
+deriveElmDef defaultOptions ''RequestNode
+
+main :: IO ()
+main =
+  let
+    options = defElmOptions
+    namespace = [ "Api" ]
+    targetFolder = "../front/client"
+    elmDefinitions = [ DefineElm (Proxy :: Proxy RequestCollection)
+                     , DefineElm (Proxy :: Proxy RequestNode)
+                     ]
+    proxyApi = (Proxy :: Proxy Api)
+  in
+    generateElmModuleWith options namespace defElmImports targetFolder elmDefinitions proxyApi
