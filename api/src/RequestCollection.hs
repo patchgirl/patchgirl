74249ecacee01c00cdeab712f556753fd18197b2
iori <matsuhar@gmail.com>
2019-11-01 08:08:26 +0100

    ++

--- api/src/RequestCollection.hs
+++ api/src/RequestCollection.hs
@@ -13,15 +13,13 @@ module RequestCollection where
 import           Control.Monad.IO.Class
 import           Data.Aeson
 import Data.Aeson.Types (parseEither)
-import           Data.Maybe
 import           Database.PostgreSQL.Simple
 import           Database.PostgreSQL.Simple.FromField
-import           Database.PostgreSQL.Simple.ToField
+import           Database.PostgreSQL.Simple.FromRow (FromRow(..))
 import           Database.PostgreSQL.Simple.SqlQQ
 import           DB
 import           GHC.Generics
 import           Servant
-import           Data.Aeson (encode)
 import Http
 
 -- * Model
@@ -55,52 +53,75 @@ instance FromField [RequestNode] where
 
 selectRequestCollectionById :: Int -> Connection -> IO (Maybe RequestCollection)
 selectRequestCollectionById requestCollectionId connection = do
-  listToMaybe <$> query connection rawQuery (Only requestCollectionId)
+  query connection collectionExistsSql (Only requestCollectionId) >>= \case
+    [Only True] -> do
+      [requestCollection] <- query connection rawQuery (requestCollectionId, requestCollectionId)
+      return $ Just requestCollection
+    _ ->
+      return Nothing
   where
+    collectionExistsSql =
+      [sql|
+          SELECT EXISTS (
+            SELECT 1
+            FROM request_collection_to_request_node
+            WHERE request_collection_id = ?
+          );
+          |]
     rawQuery =
       [sql|
-          WITH RECURSIVE request_node_from_parents AS
-          (
-            SELECT id, name, tag, '{}'::int[] AS parents, 0 AS level
-            FROM request_node
+          WITH RECURSIVE request_node_with_its_parent AS (
+            SELECT id, '{}'::int[] AS parents, 0 AS level
+            FROM request_node n
+            INNER JOIN request_collection_to_request_node cn ON(cn.request_node_id = n.id)
             WHERE request_node_parent_id IS NULL
-            AND request_collection_id = ?
-
+            AND cn.request_collection_id = ?
             UNION ALL
-
-            SELECT c.id, c.name, c.tag, parents || c.request_node_parent_id, level + 1
-            FROM request_node_from_parents p
+            SELECT c.id, parents || c.request_node_parent_id, level + 1
+            FROM request_node_with_its_parent p
             JOIN request_node c
             ON c.request_node_parent_id = p.id
-          ),
-          request_node_from_children AS
-          (
-            SELECT c.request_node_parent_id,
-            json_agg(
-              jsonb_build_object('name', c.name) || jsonb_build_object('tag', c.tag)
-            )::jsonb AS js
-            FROM request_node_from_parents tree
-            JOIN request_node c USING(id)
-            WHERE NOT id = ANY(parents) AND level > 0
-            GROUP BY c.request_node_parent_id
 
+          ), parent_ids AS (
+            SELECT DISTINCT UNNEST(parents) AS id
+            FROM request_node_with_its_parent
+
+          ), leave_ids AS (
+            SELECT r.id, level
+            FROM request_node_with_its_parent r
+            LEFT JOIN parent_ids p ON r.id = p.id
+            WHERE p.id IS NULL
+
+          ), request_node_from_leaves AS (
+            (
+              -- root leaves
+              SELECT c.request_node_parent_id,
+              request_node_as_js(c) AS js
+              FROM leave_ids l
+              JOIN request_node c USING(id)
+              WHERE level = 0
+
+              UNION ALL
+
+              -- leaves
+              SELECT c.request_node_parent_id,
+              json_agg(request_node_as_js(c))::jsonb AS js
+              FROM leave_ids l
+              JOIN request_node c USING(id)
+              WHERE level > 0
+              GROUP BY request_node_parent_id
+            )
             UNION ALL
-
             SELECT
               c.request_node_parent_id,
-              jsonb_build_object('name', c.name) ||
-              jsonb_build_object('children', js) ||
-              jsonb_build_object('tag', c.tag) AS js
-            FROM request_node_from_children tree
+              request_node_as_js(c) || jsonb_build_object('children', js) AS js
+            FROM request_node_from_leaves tree
             JOIN request_node c ON c.id = tree.request_node_parent_id
           )
-          SELECT jsonb_pretty(jsonb_agg(js))
-          FROM request_node_from_children
-          WHERE request_node_parent_id IS NULL;
 
-          SELECT id, tree
-          FROM request_collection
-          WHERE id = ?
+          SELECT ?, jsonb_pretty(jsonb_agg(js))
+          FROM request_node_from_leaves
+          WHERE request_node_parent_id IS NULL;
           |] :: Query
 
 -- * Handler
