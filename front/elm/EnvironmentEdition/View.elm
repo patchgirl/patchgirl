90d51a9e537ca77af5e40a03a727b394534e47bc
iori <matsuhar@gmail.com>
2019-11-19 17:25:48 +0100

    ++

--- front/elm/EnvironmentEdition/View.elm
+++ front/elm/EnvironmentEdition/View.elm
@@ -94,51 +94,6 @@ entryView renameEnvIdx mSelectedEnvIdx idx environment =
           }
       ]
 
-{-
-view : Model a -> Html Msg
-view model =
-    let
-        foo =
-            ul [ class "column" ] <|
-              List.indexedMap (envView model) model.environments
-        bar = (List.indexedMap (entryView model.selectedEnvironmentToRenameIndex model.selectedEnvironmentToEditIndex) model.environments)
-
-        baz = div [ onClick Add, class "centerHorizontal align-self-center" ] [ text "+" ]
-    in
-        div [ id "envApp", class "columns" ]
-          [ ul [ class "column is-offset-1 is-1" ] (bar ++ [ baz ])
-          , foo
-          ]
-
-entryView : Maybe Int -> Maybe Int -> Int -> Type.Environment -> Html Msg
-entryView renameEnvIdx mSelectedEnvIdx idx environment =
-  let
-    readView = a [ href "#", onClick (SelectEnvToEdit idx) ] [ span [] [ text environment.name ] ]
-    editView = input [ value environment.name, Util.onEnterWithInput (Rename idx) ] []
-    modeView =
-      case renameEnvIdx == Just idx of
-        True -> editView
-        False -> readView
-    active =
-        case mSelectedEnvIdx == Just idx of
-            True -> "active"
-            False -> ""
-  in
-    li [ class active ]
-      [ modeView
-      , a [ href "#", onClick (ShowRenameInput idx)] [ text " Rename" ]
-      , a [ href "#", onClick (Delete idx)] [ text " -" ]
-      ]
-
-envView : Model a -> Int -> Type.Environment -> Html Msg
-envView model idx environment =
-    let
-        isEnvSelected = model.selectedEnvironmentToEditIndex == Just idx
-    in
-        div [ hidden (not isEnvSelected) ]
-            [ Html.map (EnvironmentKeyValueEditionMsg idx) (EnvironmentKeyValueEdition.view environment.keyValues) ]
--}
-
 labelInputView : String -> Input.Label Msg
 labelInputView labelText =
     let
