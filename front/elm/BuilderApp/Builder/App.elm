1865042473f519da11d6aacd0fc9081308417992
iori <matsuhar@gmail.com>
2020-01-02 11:31:56 +0100

    ++

--- front/elm/BuilderApp/Builder/App.elm
+++ front/elm/BuilderApp/Builder/App.elm
@@ -8,7 +8,6 @@ import Json.Decode as Json
 import List.Extra as List
 import Combine as Combine
 
-import BuilderApp.Builder.Model exposing (..)
 import Api.Generated as Client
 import Maybe.Extra as Maybe
 import Application.Type exposing (..)
@@ -26,11 +25,73 @@ import Html as Html
 import Html.Attributes as Html
 import Html.Events as Html
 
-import BuilderApp.Builder.Model exposing (..)
 import Util.View as Util
 import Application.Type exposing (..)
 import Dict as Dict
 import Json.Print as Json
+
+
+-- * model
+
+
+type alias Model a =
+    { a
+        | name : Editable String
+        , httpUrl : Editable String
+        , httpMethod : Editable Client.Method
+        , httpHeaders : Editable (List (String, String))
+        , httpBody : Editable String
+        , response : Maybe Response
+        , showResponseView : Bool
+    }
+
+isBuilderDirty : Model a -> Bool
+isBuilderDirty model =
+    isDirty model.httpMethod ||
+        isDirty model.httpHeaders ||
+            List.any isDirty [model.name, model.httpUrl, model.httpBody]
+
+
+type alias Response =
+    { statusCode : Int
+    , statusText : String
+    , headers : Dict.Dict String String
+    , body : String
+    }
+
+type Status
+    = Success
+    | Failure
+
+type ErrorDetailed
+    = BadUrl String
+    | Timeout
+    | NetworkError
+    | BadStatus Http.Metadata String
+
+defaultBuilder =
+  { httpUrl = NotEdited ""
+  , httpMethod = Client.Get
+  , httpHeaders = NotEdited []
+  , httpBody = NotEdited ""
+  , response = Nothing
+  }
+
+defaultModel1 =
+  { httpUrl = NotEdited "{{url}}/api/people/1"
+  , httpMethod = Client.Get
+  , httpHeaders = NotEdited []
+  , httpBody = NotEdited ""
+  , response = Nothing
+  }
+
+defaultModel =
+  { httpUrl = NotEdited "swapi.co/api/people/2"
+  , httpMethod = Client.Get
+  , httpHeaders = NotEdited []
+  , httpBody = NotEdited ""
+  , response = Nothing
+  }
 
 
 -- * message
