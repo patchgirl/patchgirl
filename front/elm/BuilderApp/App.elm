29a2916a712e3d3b9527da4037f7fe2e93ac1307
iori <matsuhar@gmail.com>
2020-02-26 11:21:35 +0100

    remove unusued function

--- front/elm/BuilderApp/App.elm
+++ front/elm/BuilderApp/App.elm
@@ -103,27 +103,6 @@ update msg model =
 -- * util
 
 
-findPrevious : List a -> a -> Maybe a
-findPrevious l a =
-    case l of
-        x :: y :: z ->
-            if a == x then
-                Just y
-            else if a == y then
-                Just x
-            else
-                findPrevious (y :: z) a
-        x :: xs -> findPrevious xs a
-        [] -> Nothing
-
-markFileAsSaved : RequestNode -> RequestNode
-markFileAsSaved node =
-  case node of
-      RequestFolder f ->
-          RequestFolder f
-      RequestFile f ->
-          RequestFile { f | isSaved = True }
-
 changeFileBuilder : File -> RequestNode -> RequestNode
 changeFileBuilder newFile node =
     case node of
