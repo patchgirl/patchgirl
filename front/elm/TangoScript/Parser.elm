f087288ac4647b03d1ea62328d201a7fbe51217e
iori <matsuhar@gmail.com>
2020-05-01 15:13:49 +0200

    f: tango better ast

--- front/elm/TangoScript/Parser.elm
+++ front/elm/TangoScript/Parser.elm
@@ -5,25 +5,6 @@ import Parser.Expression as P exposing (OperatorTable)
 import Set exposing (Set)
 
 
--- * AST
-
-
-type Expr
-  = Lit Lit
-  | Op Binop Expr Expr
-  | AssertEqual Expr Expr
-  | Let String Expr
-  | Var String
-
-type Lit
-  = LInt Int
-  | LBool Bool
-
-type Binop
-    = Add
-    | Sub
-
-
 -- * reserved keywords
 
 
@@ -31,18 +12,123 @@ reserved : Set String
 reserved =
     Set.fromList
         [ "assertEqual"
-        , "let"
+        , "var"
         , "true"
         , "false"
         ]
 
 
+-- * proc
+
+
+type Proc
+    = AssertEqual Expr Expr
+    | Let String Expr
+
+procParser : Parser Proc
+procParser =
+    P.succeed identity
+        |. P.spaces
+        |= P.oneOf
+           [ assertEqualParser
+           , letParser
+           ]
+        |. P.spaces
+
+
+-- ** let
+
+
+letParser : Parser Proc
+letParser =
+    P.succeed Let
+        |. P.keyword "var"
+        |. P.spaces
+        |= variableNameParser
+        |. P.spaces
+        |. P.symbol "="
+        |. P.spaces
+        |= (P.lazy <| \_ -> exprParser)
+
+
+-- ** assert equal
+
+
+assertEqualParser : Parser Proc
+assertEqualParser =
+    P.succeed AssertEqual
+        |. P.keyword "assertEqual"
+        |. P.spaces
+        |. P.symbol "("
+        |. P.spaces
+        |= (P.lazy <| \_ -> exprParser)
+        |. P.spaces
+        |. P.symbol ","
+        |. P.spaces
+        |= (P.lazy <| \_ -> exprParser)
+        |. P.spaces
+        |. P.symbol ")"
+
+
+-- * expr
+
+
+type Expr
+    = LBool Bool
+    | LInt Int
+    | Var String
+
+
+exprParser : Parser Expr
+exprParser =
+    let
+        lIntParser =
+            P.succeed LInt
+                |= P.int
+
+        lBoolParser =
+            P.succeed LBool
+                |= P.oneOf [ P.map (always True) (P.keyword "true")
+                           , P.map (always False) (P.keyword "false")
+                           ]
+    in
+    P.succeed identity
+        |. P.spaces
+        |= P.oneOf
+           [ lBoolParser
+           , lIntParser
+           , varParser
+           ]
+        |. P.spaces
+
+
 -- * parser
 
 
+type alias TangoAst = List Proc
+
+tangoParser : Parser TangoAst
+tangoParser =
+    let
+        lineHelper : List Proc -> Parser (Step TangoAst TangoAst)
+        lineHelper revStmts =
+            P.oneOf
+                [ P.succeed (\stmt -> P.Loop (stmt :: revStmts))
+                    |= procParser
+                    |. P.spaces
+                    |. P.symbol ";"
+                    |. P.spaces
+                , P.succeed ()
+                    |> P.map (\_ -> P.Done (List.reverse revStmts))
+                ]
+
+    in
+    P.succeed identity
+        |= P.loop [] lineHelper
+
 -- ** expr
 
-
+{-
 parser : Parser (List Expr)
 parser =
     let
@@ -89,30 +175,7 @@ exprParser =
            , litParser
            ]
         |. P.spaces
-
-
--- ** litteral
-
-
-litParser : Parser Expr
-litParser =
-    let
-        lIntParser =
-            P.succeed (Lit << LInt)
-                |. P.spaces
-                |= P.int
-                |. P.spaces
-
-        lBoolParser =
-            P.succeed (Lit << LBool)
-                |. P.spaces
-                |= P.oneOf [ P.map (always True) (P.symbol "true")
-                           , P.map (always False) (P.symbol "false")
-                           ]
-                |. P.spaces
-    in
-    P.oneOf [ lIntParser, lBoolParser ]
-
+-}
 
 -- ** variable
 
@@ -130,30 +193,15 @@ varParser =
     P.map Var variableNameParser
 
 
--- ** let
-
-
-letParser : Parser Expr
-letParser =
-    P.succeed Let
-        |. P.keyword "let" |. P.spaces
-
-        |= variableNameParser |. P.spaces
-
-        |. P.symbol "=" |. P.spaces
-
-        |= (P.lazy <| \_ -> exprParser)
-
-
 -- ** bin operator
 
-
+{-
 binOpParser : Parser Expr
 binOpParser =
     let
         operators : List (List (P.Operator Expr))
         operators =
-            [ [ P.infixOperator (\a b -> Op Add a b) (P.symbol "+") P.AssocLeft
+            [ [ P.infixOperator (\a b -> BinOp Add a b) (P.symbol "+") P.AssocLeft
               ]
             ]
 
@@ -170,16 +218,4 @@ binOpParser =
                     |. P.spaces
                     |= P.lazy (\_ -> operandParser)
                     |. P.spaces
-
--- ** assert equal
-
-
-assertEqualParser : Parser Expr
-assertEqualParser =
-    P.succeed AssertEqual
-        |. P.keyword "assertEqual" |. P.spaces
-        |. P.symbol "(" |. P.spaces
-        |= (P.lazy <| \_ -> exprParser) |. P.spaces
-        |. P.symbol "," |. P.spaces
-        |= (P.lazy <| \_ -> exprParser) |. P.spaces
-        |. P.symbol ")"
+-}
--- front/tests/TangoScript/ParserTest.elm
+++ front/tests/TangoScript/ParserTest.elm
@@ -6,182 +6,188 @@ import TangoScript.Parser exposing(..)
 import Parser as P
 
 
+-- * expr
+
+
+lint : List (ParserTest (List P.DeadEnd) Expr)
+lint =
+    [ { message = "parse one digit integer"
+      , input = "1"
+      , expect = Ok <| LInt 1
+      }
+    , { message = "parse multi digits integer"
+      , input = "123"
+      , expect = Ok <| LInt 123
+      }
+    ]
+
+lbool : List (ParserTest (List P.DeadEnd) Expr)
+lbool =
+    [ { message = "parse true value"
+      , input = "true"
+      , expect = Ok <| LBool True
+      }
+    , { message = "parse false value"
+      , input = "false"
+      , expect = Ok <| LBool False
+      }
+    , { message = "don't parse string that looks like boolean"
+      , input = "falsey"
+      , expect = Ok <| Var "falsey"
+      }
+    , { message = "don't parse case doesn't match"
+      , input = "falsE"
+      , expect = Ok <| Var "falsE"
+      }
+    ]
+
+lvar : List (ParserTest (List P.DeadEnd) Expr)
+lvar =
+    [ { message = "parse var"
+      , input = "yes"
+      , expect = Ok <| Var "yes"
+      }
+    , { message = "dont parse var that start with capital case"
+      , input = "Yes"
+      , expect = Err []
+      }
+    , { message = "dont parse var that start with number"
+      , input = "1yes"
+      , expect = Ok <| LInt 1
+      }
+    , { message = "parse var with weird format"
+      , input = "yes01T_iset"
+      , expect = Ok <| Var "yes01T_iset"
+      }
+    , { message = "parse only var before hyphen"
+      , input = "foo-bar"
+      , expect = Ok <| Var "foo"
+      }
+    ]
+
+
+-- * proc
+
+
+letTests : List (ParserTest (List P.DeadEnd) Proc)
+letTests =
+    [ { message = "parse simple let"
+      , input = "var a = 1"
+      , expect = Ok <| Let "a" (LInt 1)
+      }
+    ]
+
+assertEqualTests : List (ParserTest (List P.DeadEnd) Proc)
+assertEqualTests =
+    [ { message = "parse simple assertEqual"
+      , input = "assertEqual(1,1)"
+      , expect = Ok <| AssertEqual (LInt 1) (LInt 1)
+      }
+    , { message = "parse assertEqual with spaces"
+      , input = "assertEqual ( 1 , 1 ) "
+      , expect = Ok <| AssertEqual (LInt 1) (LInt 1)
+      }
+    , { message = "parse assertEqual with int and bool as param"
+      , input = "assertEqual( 1 , true )"
+      , expect = Ok <| AssertEqual (LInt 1) (LBool True)
+      }
+    ]
+
+
+-- * tango
+
+
+tangoTests : List (ParserTests (List P.DeadEnd) TangoAst)
+tangoTests =
+    [ { message = "parse simple statement"
+      , input = "assertEqual(1,1);"
+      , expect = Ok <| [ AssertEqual (LInt 1) (LInt 1) ]
+      }
+    , { message = "dont parse statement that doesnt end with semi-colon"
+      , input = "assertEqual(1,1)"
+      , expect = Err [{ col = 17, problem = P.ExpectingSymbol ";", row = 1 }]
+      }
+    , { message = "dont parse expression"
+      , input = "1;"
+      , expect = Ok []
+      }
+    , { message = "parse more complex statements"
+      , input = """
+                 var a = 1;
+                 var b = 2;
+                 assertEqual(a, b);
+                """
+      , expect = Ok [ Let "a" (LInt 1)
+                    , Let "b" (LInt 2)
+                    , AssertEqual (Var "a") (Var "b")
+                    ]
+      }
+    ]
+
+
+-- * suite
+
+
 suite : Test
 suite =
     describe "Parser module"
-
-
--- * mExpr parser
-
-
-        [ describe "mExprParser"
-            [ test "don't parse when there's nothing to parse" <|
-                  \_ ->
-                  let
-                      inputWithNewlines = """
-
-
-                                          """
-                      inputs = [ "", " ", inputWithNewlines ]
-                      expectedRes = [ Ok Nothing, Ok Nothing, Ok Nothing ]
-                  in
-                  Expect.equal (List.map (P.run mExprParser) inputs) expectedRes
+        [ describe "ExprParser"
+            [ describe "LInt" <| List.map checkExprParser lint
+            , describe "LBool" <| List.map checkExprParser lbool
+            , describe "Var" <| List.map checkExprParser lvar
             ]
-
-
--- * expr parser
-
-
-        , describe "ExprParser"
-
-
--- ** litteral
-
-
-            [ test "LInt" <|
-                  \_ ->
-                  let
-                      input = "1;"
-                      expectedRes = Lit (LInt 1)
-                  in
-                  Expect.equal (P.run exprParser input) (Ok expectedRes)
-
-            , test "LBool" <|
-                  \_ ->
-                  let
-                      inputs = ["true;", "false;", " true  ;"]
-                      expectedRes =
-                          [ Ok (Lit (LBool True))
-                          , Ok (Lit (LBool False))
-                          , Ok (Lit (LBool True))
-                          ]
-                  in
-                  Expect.equal expectedRes (List.map (P.run exprParser) inputs)
-
-
--- ** bin op
-
-
-            , test "add op" <|
-                  \_ ->
-                  let
-                      inputs = ["1+1;", "1 + 1;", "1 + true ;"]
-                      expectedRes =
-                          [ Ok <| Op Add (Lit (LInt 1)) (Lit (LInt 1))
-                          , Ok <| Op Add (Lit (LInt 1)) (Lit (LInt 1))
-                          , Ok <| Op Add (Lit (LInt 1)) (Lit (LBool True))
-                          ]
-
-                  in
-                  Expect.equal (List.map (P.run exprParser) inputs) expectedRes
-
--- ** assert equal
-
-
-            , test "assert equal" <|
-                  \_ ->
-                  let
-                      inputs =
-                          [ "assertEqual(1,1);"
-                          , "assertEqual( 1 , 1 );"
-                          , "assertEqual( 1 , true );"
-                          , "assertEqual( 1 , 1 + 1 );"
-                          ]
-                      expectedRes =
-                          [ Ok <| AssertEqual (Lit (LInt 1)) (Lit (LInt 1))
-                          , Ok <| AssertEqual (Lit (LInt 1)) (Lit (LInt 1))
-                          , Ok <| AssertEqual (Lit (LInt 1)) (Lit (LBool True))
-                          , Ok <| AssertEqual (Lit (LInt 1)) (Op Add (Lit (LInt 1)) (Lit (LInt 1)))
-                          ]
-
-                  in
-                  Expect.equal (List.map (P.run exprParser) inputs) expectedRes
-
-
-
--- ** var
-
-
-            , test "var" <|
-                  \_ ->
-                  let
-                      inputs =
-                          [ "foo"
-                          ]
-                      expectedRes =
-                          [ Ok <| Var "foo"
-                          ]
-
-                  in
-                  Expect.equal (List.map (P.run exprParser) inputs) expectedRes
-
-
--- ** var assoc
-
-
-            , test "var assoc" <|
-                  \_ ->
-                  let
-                      inputs =
-                          [ "let a = 1;"
-                          , "let a = 1 + 1;"
-                          , "let a = 1 + false;"
-                          , "let a = 1 + foo;"
-                          ]
-                      expectedRes =
-                          [ Ok <| Let "a" (Lit (LInt 1))
-                          , Ok <| Let "a" (Op Add (Lit (LInt 1)) (Lit (LInt 1)))
-                          , Ok <| Let "a" (Op Add (Lit (LInt 1)) (Lit (LBool False)))
-                          , Ok <| Let "a" (Op Add (Lit (LInt 1)) (Var "foo"))
-                          ]
-
-                  in
-                  Expect.equal (List.map (P.run exprParser) inputs) expectedRes
-
-                      ]
-
-
--- * main parser
-
-
-        , describe "MainParser"
-
-
--- ** parser
-
-
-            [ test "app" <|
-                  \_ ->
-                  let
-                      inputs =
-                          [ """
-                            1 ; 1;
-                            """
-                          , """
-                            1 ; 1
-                             ;
-                            """
-                          , """
-                             let a = 1;
-                             let b = 2;
-                             a + b;
-                            """
-                          ]
-                      expectedRes =
-                          [ Ok <| [ (Lit (LInt 1))
-                                  , (Lit (LInt 1))
-                                  ]
-                          , Ok <| [ (Lit (LInt 1))
-                                  , (Lit (LInt 1))
-                                  ]
-                          , Ok <| [ Let "a" (Lit (LInt 1))
-                                  , Let "b" (Lit (LInt 2))
-                                  , Op Add (Var "a") (Var "b")
-                                  ]
-                          ]
-
-                  in
-                  Expect.equal (List.map (P.run parser) inputs) expectedRes
-
+        , describe "ProcParser"
+            [ describe "Let" <| List.map checkProcParser letTests
+            , describe "AssertEqual" <| List.map checkProcParser assertEqualTests
             ]
+        , describe "TangoParser"
+            <| List.map checkTangoAstParser tangoTests
         ]
+
+
+-- * util
+
+
+type alias ParserTest error a =
+    { message : String
+    , input : String
+    , expect : Result error a
+    }
+
+checkExprParser : ParserTest (List P.DeadEnd) Expr -> Test
+checkExprParser { message, input, expect } =
+    test message <|
+        \_ ->
+            case P.run exprParser input of
+                Ok _ as ok ->
+                    Expect.equal ok expect
+
+                Err _ ->
+                    Expect.equal (Err []) expect
+
+checkProcParser : ParserTest (List P.DeadEnd) Proc -> Test
+checkProcParser { message, input, expect } =
+    test message <|
+        \_ ->
+            case P.run procParser input of
+                Ok _ as ok ->
+                    Expect.equal ok expect
+
+                Err _ ->
+                    Expect.equal (Err []) expect
+
+type alias ParserTests error a =
+    { message : String
+    , input : String
+    , expect : Result error a
+    }
+
+checkTangoAstParser : ParserTests (List P.DeadEnd) TangoAst -> Test
+checkTangoAstParser { message, input, expect } =
+    test message <|
+        \_ ->
+            let
+                a : Result (List P.DeadEnd) TangoAst
+                a = P.run tangoParser input
+            in
+            Expect.equal (P.run tangoParser input) expect
