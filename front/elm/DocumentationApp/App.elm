0e556f15983d8972d6bc58bf21e014c0142f9dd9
iori <matsuhar@gmail.com>
2020-06-11 16:25:01 +0200

    documentation++

--- front/elm/DocumentationApp/App.elm
+++ front/elm/DocumentationApp/App.elm
@@ -67,7 +67,7 @@ contentView documentation =
     el [ width (fill |> maximum 800), centerX ] <|
         case documentation of
             RequestDoc ->
-                column [] requestView
+                column [] (renderMarkdown requestView)
 
             ScenarioDoc ->
                 none
@@ -81,9 +81,7 @@ contentView documentation =
 
 requestView : List (Element msg)
 requestView =
-    let
-        markdown =
-            """
+    """
 # Request
 
 The "Request" menu allows you to create and run HTTP request.
@@ -92,16 +90,15 @@ The "Request" menu allows you to create and run HTTP request.
 
 Variables are available through the mustache syntax.
 
-If you have variable `userId` in your environment you use can use it in any text input with `{{userId}}`
+If you have variable `userId` set in your environment you can use it in any text input with `{{userId}}`.
 
 ## Limitations
 
 Due to browser restrictions (cf: [CORS](https://developer.mozilla.org/docs/Web/HTTP/CORS) and [same origin policy](https://developer.mozilla.org/docs/Web/Security/Same_origin_policy_for_JavaScript)), PatchGirl cannot run from your device.
 Instead, every requests you make are being sent from our servers which means that you can't by default query local hosts (eg: 127.0.0.1) or use your personnal VPN/Proxy.
-To overcome this issue, you need to download the **PatchGirl Desktop App** which runs on your computer and allows you to run requests from your device.
+To overcome this issue, you need to download and run the **PatchGirl Desktop App** (only available for Linux at the time) which runs on your computer and allows you to run requests from your device.
+The PatchGirl desktop app acts as a tiny proxy. You still use the web app but every calls are being sent from the desktop app instead of our servers.
     """
-    in
-    renderMarkdown markdown
 
 
 -- ** scenario
@@ -109,7 +106,46 @@ To overcome this issue, you need to download the **PatchGirl Desktop App** which
 
 scenarioView : Element msg
 scenarioView =
-    text "Scen Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker."
+    """
+# Scenario
+
+The "Scenario" menu allows you to sequentially play HTTP requests.
+
+## Http Requests
+
+Once your http requests are created, they can be added to your scenario.
+
+## [Pre|Post] Script
+
+You can add script before and after every http requests of a scenario. This might come in handy if you need to override a variable before executing a request or if you need to test the response of a request.
+
+### Local variable
+
+In your script, you can define local variable with the syntax `var foo = 1;`
+Local variable's scope is only limited to the script they are being defined in.
+
+### Global variable
+
+You can define a variable that will live through out its script with the syntax: `set("foo", 1);`
+You can use a global variable with this syntax: `get("foo");`
+Note that global variable's scope is only limited to the scenario they are defined in.
+For variable that lives everywhere, you need to use environment variables.
+
+### Assertion
+
+When you run a scenario, you can make assertions in a script with the `assertEqual` function. Its syntax is `assertEqual("someString", "someOtherString");`. If both parameter are of the same type and same value, then the script succeeds.
+When an assertion fails, every following scenes won't be ran.
+
+In a post script, some utilities are available:
+- `httpResponseBodyAsString` returns the response body as a string
+- `httpResponseStatus` returns the response status as an int
+
+So if you want to check that the response succeeded you can write: `assertEqual(httpResponseStatus, 200);`
+
+
+## Limitations
+
+"""
 
 
 -- ** environment
@@ -117,7 +153,17 @@ scenarioView =
 
 environmentView : Element msg
 environmentView =
-    text "Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker."
+    """
+# Environment variables
+
+The environment menu allows you to create variables that you can reuse in your http request.
+
+## use variable
+
+Once defined, a variable can easily be used in an http request text inputs or in a scenario's script.
+The syntax used is the mustache syntax, e.g: `http://{{host}}/somePath`
+
+     """
 
 
 -- * markdown
